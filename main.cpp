810411bd012ffa6bc298838f62b11d74383c1d7d
jzepedai <jzepeda@ideasreward.com>
2018-08-13 11:56:22 -0500

    adding test01

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+
+
+int main(){
+
+	ofstream archivo("prueba06.dat",ios::out | ios::binary);
+	unsigned t0, t1;
+	t0=clock();
+
+
+ 	//archivo.open("C:\\Users\\Julián\\Desktop\\Blockchain\\prueba06.dat");
+    if(!archivo){
+        cerr << "No se pudo abrir el archivo."<<endl;
+    }
+
+ 	for (int i=0; i<136637; i++){
+    	archivo << "qqqq 3 " << endl;
+    //	archivo << i << endl;
+    	//archivo << hash << endl;
+
+	}
+
+    archivo.close();
+    t1 = clock();
+    double time = (double(t1-t0)/CLOCKS_PER_SEC);
+	cout << "Execution Time: " << time << endl;
+
+	return 0;
+}
